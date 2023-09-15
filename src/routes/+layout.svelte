0d278c711ec2b879ca7a1aeb2b5bc1525012c365
jycouet <jycouet@gmail.com>
2023-09-15 09:17:10 +0200

    init

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,7 @@
+<a href="/">Home</a> |
+<a href="/page-a">Page A</a> |
+<a href="/page-b">Page B</a>
+
+<hr />
+
+<slot />
---
+++ src/routes/page-a/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import { page } from "$app/stores";
+  import { queryParameters, ssp } from "sveltekit-search-params";
+  const store_qp = queryParameters({
+    withOne: ssp.boolean(true),
+    withTwo: ssp.boolean(false),
+
+    pushHistory: false,
+  });
+</script>
+
+My queryParameters: <pre>{$page.url.searchParams}</pre>
